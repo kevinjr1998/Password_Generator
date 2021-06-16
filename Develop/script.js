8115392e093dbb6134e533a5b9512f80f0da9905
Kevin98 <kevinryner@yahoo.co.uk>
2021-06-16 19:01:33 +0100

    declared and coded function to create password

--- Develop/script.js
+++ Develop/script.js
@@ -14,38 +14,47 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+// generate password
+generatePassword(){
 
-
-
-generatePassword()
-{
-  var lowercase = false;
-  var uppercase = false;
-  var numeric = false;
-  var special = false;
-  var specialCharacters = '!#$%&()*+,-./<=>?@[\]^_{|}~'; 
-  var numericCgharacters = '0123456789';
-  var lowercaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
-  var uppercaseCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var lowercaseCharacters = false;
+  var uppercaseCharacters = false;
+  var numericCharacters = false;
+  var specialCharacters = false;
+  var specialCharactersList = '!#$%&()*+,-./<=>?@[\]^_{|}~'; 
+  var numericCgharactersList = '0123456789';
+  var lowercaseCharactersList = 'abcdefghijklmnopqrstuvwxyz';
+  var uppercaseCharactersList = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var characters = '';
+  var passwordLength;
 
-  if (!lowercase && !uppercase && !numeric && !special) {
-    alert("Please select an option!");
+  if (!lowercaseCharacters && !uppercaseCharacters && !numericCharacters && !specialCharacters) {
+    alert("You must select at least one option!");
     return;
   };
 
-  if (uppercase) {
-    characters += uppercaseCharacters;
+  if (uppercaseCharacters) {
+    characters += uppercaseCharactersList;
   };
-  if (lowercase) {
-    characters += lowercaseCharacters;
+
+  if (lowercaseCharacters) {
+    characters += lowercaseCharactersList;
   };
-  if () {};
-  if () {};
 
+  if (specialCharacters) {
+    characters += specialCharactersList;
+  };
 
+  if (numericCharacters) {
+    characters += specialCharacters;
+  };
 
+  for (i=0; i < passwordLength; i++){
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
 
-}
+  return result;
+
+  }
 
 
